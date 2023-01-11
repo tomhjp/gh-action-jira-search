1f4eef2d99fcc50994d88df3838a8605699c8df6
Dave Parfitt <dave.parfitt@hashicorp.com>
2023-01-11 15:15:54 -0500

    update to use new method to set output
    save-state and set-output are deprecated per
    https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- main.go
+++ main.go
@@ -44,8 +44,8 @@ func search() error {
 	fmt.Printf("Found issue %s\n", key)
 
 	// Special format log line to set output for the action.
-	// See https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#outputs-for-composite-run-steps-actions.
-	fmt.Printf("::set-output name=key::%s\n", key)
+	// See https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+	fmt.Printf("key=%s >> $GITHUB_OUTPUT\n", key)
 
 	return nil
 }
